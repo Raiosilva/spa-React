b5347262ff5a87e3d985d7e40eccad65460e56e4
Raiosilva <raimundopi025@gmail.com>
2019-07-05 10:28:36 -0300

    adding HOmePageContainer, index.css

--- src/containers/HomePageContainer/index.js
+++ src/containers/HomePageContainer/index.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-
+import HomePage from '../../components/HomePage';
 class HomePageContainer extends Component {
  render() {
   return (
-   <div>
-     Home Page 
-   </div>
+     <HomePage/>
   );
  }
 }
